fc6e0e675f32f7fd4610de8fdb4c54c9acc0843e
jdabassett <jacobbassett@gmail.com>
2024-02-13 17:11:31 -0700

    context mostly inplace"

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import "@/styles/globals.css";
 
+import AuthProvider from "@/contexts/AuthContext";
+import ErrorProvider from "@/contexts/ErrorContext";
+import PageProvider from "@/contexts/PageContext";
+import PromptProvider from "@/contexts/PromptContext";
+
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+
+  return <>
+          <ErrorProvider>
+            <AuthProvider>
+              <PageProvider>
+                <PromptProvider>
+                  <Component {...pageProps} />
+                </PromptProvider>
+              </PageProvider>
+            </AuthProvider>;
+          </ErrorProvider>
+        </>
+};
