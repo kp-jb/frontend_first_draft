92252696dc0221dcc1e5898ce1ca49a9321ea0ae
Kiengchay Gomez <kphoutthavong@gmail.com>
2024-02-21 16:04:45 -0600

    add google fonts; copyright to footer

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import { useAuthContext } from "@/contexts/AuthContext";
 import {useRouter} from "next/router";
+import Head from "next/head";
 
 export default function Header(props) {
   let { tokens, userData, logoutFunction } = useAuthContext();
@@ -9,34 +10,48 @@ export default function Header(props) {
   //   console.log("Header", tokens);
   // }
   return(
-    <header className="text-red-500">
-      <h1 
-        className=""
-        >First Draft</h1>
-      <p>Header</p>
-      {userData
-        ?
-        <div>
-          <button 
-            className="">
-           {userData.first_name}
-          </button>
-          <button 
-            className=""
-            onClick={logoutFunction}>
-            Sign Out
-          </button>
+      <>
+        <Head>
+          <title>First Draft</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" rel="stylesheet" />
+      </Head>
+      <div className="flex flex-row">
+        <header className="m-3 text-3xl text-gray-200">
+        <h1 
+          className="font-sans">
+            First Draft
+        </h1>
+        <div className="flex gap-3 text-xs">
+          <p>Header</p>
+        {userData
+          ?
+          <div>
+            <button 
+              className="">
+            {userData.first_name}
+            </button>
+            <button 
+              className=""
+              onClick={logoutFunction}>
+              Sign Out
+            </button>
+          </div>
+          :
+          <div>
+            <button 
+              className=""
+              onClick={()=>router.push("/login")}
+              >
+              Login
+            </button>
+          </div>
+        }
         </div>
-        :
-        <div>
-          <button 
-            className=""
-            onClick={()=>router.push("/login")}
-            >
-            Login
-          </button>
-        </div>
-      }
-    </header>
+      </header>
+      </div>
+      
+    </>
   );
 }
--- components/Layout.js
+++ components/Layout.js
@@ -8,11 +8,11 @@ export default function Layout(props) {
   return <div className="h-full font-mono text-xs text-gray-800 bg-cover bg-corona-fore-typewriter">
             <Header></Header>
             <div className="flex w-full border-red-500">
-              <div className="w-1/6 border border-purple-500">
+              <div className="w-1/6 border border-purple-700">
                 <NavSideBar></NavSideBar>
               </div>
 
-              <div className="w-3/6 p-5 ml-4 border border-blue-500 rounded-lg bg-opacity-35 bg-slate-100">
+              <div className="w-3/6 p-5 ml-4 border rounded-lg border-white-500 bg-opacity-35 bg-slate-100">
                 {props.children}
 
               </div>
